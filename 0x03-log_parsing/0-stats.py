2a4340af4ca6cebf73844deea763ea020849c6c3
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-06 10:23:39 +0300

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,49 @@
 #!/usr/bin/python3
-
-import sys
-
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+"""A script that reads stdin line by line and computes metrics."""
+from contextlib import suppress
+from sys import stdin
+from typing import Dict, List, Tuple
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+if __name__ == '__main__':
+    StatusCodes = Dict[int, int]
+    FileSize = List[int]
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+    def _print_stats(file_size: FileSize, status_codes: StatusCodes) -> None:
+        """Private method to print stats."""
+        print('File size: {}'.format(file_size[0]))
+        for key, val in status_codes.items():
+            if val != 0:
+                print('{}: {}'.format(key, val))
 
-        if len(parsed_line) > 2:
-            counter += 1
+    def _parse_line(
+        line: str,
+        status_codes: StatusCodes,
+        file_size: FileSize
+    ) -> Tuple[StatusCodes, FileSize]:
+        """A private method to parse lines."""
+        with suppress(BaseException):
+            line = line[:-1]
+            word = line.split(' ')
+            file_size[0] += int(word[-1])
+            status_code = int(word[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        return status_codes, file_size
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    line_number = 1
+    try:
+        for line in stdin:
+            status_codes, file_size = _parse_line(
+                line, status_codes, file_size
+            )
+            if line_number % 10 == 0:
+                _print_stats(file_size, status_codes)
+            line_number += 1
+    except KeyboardInterrupt:
+        _print_stats(file_size, status_codes)
+        raise
+    _print_stats(file_size, status_codes)
